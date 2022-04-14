02e880e58ee0786710b2e78a9c134ab44ca48d64
Sami Alhamad <98410836+theabsolutedude@users.noreply.github.com>
2022-04-14 23:00:58 +0300

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -10,13 +10,24 @@ const state = {
 
 const connect = (cb) =>{
   if(state.db)
-    cb(err);
-  else{
-    state.dbn = client.db(dbname);
     cb();
+  else{
+    MongoClient.connect(url,mongoOptions,(err,client)=>{
+      if(err)
+          cb(err);
+        else{
+          state.db = client.db(dbname);
+          cb();
+        });
   }
 }
 
 const getPrimaryKey = (_id)=>{
   return ObjectID(_id);
 }
+
+const getDB = ()=>{
+  return state.db;
+}
+
+module.exports = {getDB,connect,getPrimaryKey};
