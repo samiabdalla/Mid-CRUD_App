1a68b0903faf0a801e1c263fbfcc293465f2d04f
Sami Alhamad <98410836+theabsolutedude@users.noreply.github.com>
2022-04-15 16:28:53 +0300

    Update app.js
    Server-side delete portion

--- app.js
+++ app.js
@@ -44,6 +44,16 @@ app.post('/',(req,res)=>{
   });
 });
 
+app.delete('/:id',(req,res)=>{
+  const todoID = req.params.id;
+  db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
+    if(err)
+      console.log(err);
+    else
+      res.json(result);
+  });
+});
+
 db.connect((err)=>{
   if(err) {
     console.log('Unable to connect to database');
